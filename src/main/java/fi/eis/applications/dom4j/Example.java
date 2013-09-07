34fbc88fb52b854e9f0b7050cb45cc334629e921
eis <eis@iki.fi>
2013-09-07 17:52:09 +0300

    formatted code

--- src/main/java/fi/eis/applications/dom4j/Example.java
+++ src/main/java/fi/eis/applications/dom4j/Example.java
@@ -14,34 +14,35 @@ import org.joda.time.DateTime;
 @SuppressWarnings("unchecked")
 public class Example {
 	public static void main(String args[]) throws Exception {
-		
+
 		System.out.println("Running");
-		
+
 		// Create a null Document Object
 		Document theXML = null;
 
 		// Get the document of the XML and assign to Document object
 		theXML = fromUrl("http://www.iii.co.uk/rss/news/cotn%3aAAU.L.xml");
 
-
 		// Place the root element of theXML into a variable
-		List<? extends Node> items = (List<? extends Node>)theXML.selectNodes("//rss/channel/item");
-		
+		List<? extends Node> items = (List<? extends Node>) theXML
+				.selectNodes("//rss/channel/item");
+
 		// RFC-dictated date format used with RSS
-		DateFormat dateFormatterRssPubDate = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-		
+		DateFormat dateFormatterRssPubDate = new SimpleDateFormat(
+				"EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+
 		// today started at this time
 		DateTime timeTodayStartedAt = new DateTime().withTimeAtStartOfDay();
-		
-		for (Node node: items) {
-			 String pubDate = node.valueOf( "pubDate" );
-			 DateTime date = new DateTime(dateFormatterRssPubDate.parse(pubDate));
-			 if ((date).isAfter(timeTodayStartedAt)) {
-				 // it's today, do something!
-				 System.out.println("Today: " + date);
-			 } else {
-				 System.out.println("Not today: " + date);
-			 }
+
+		for (Node node : items) {
+			String pubDate = node.valueOf("pubDate");
+			DateTime date = new DateTime(dateFormatterRssPubDate.parse(pubDate));
+			if ((date).isAfter(timeTodayStartedAt)) {
+				// it's today, do something!
+				System.out.println("Today: " + date);
+			} else {
+				System.out.println("Not today: " + date);
+			}
 		}
 
 	}
